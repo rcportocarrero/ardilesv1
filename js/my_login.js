eb7489628839fb91ae6cd26c144e0442b529c4d2
Roy <rcportocarrero@gmail.com>
2016-01-05 10:37:19 -0500

    fix borrando codigo basura

---
+++ js/my_login.js
@@ -0,0 +1,9 @@
+$('#formulario').submit(function () {
+
+    var datosUsuario = $("#txtUsuario").val();
+    var datosPassword = $("#txtClave").val();
+    
+    $.mobile.changePage("#menupri");
+
+
+});
